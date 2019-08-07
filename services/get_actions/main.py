f4de5119a4f71eb824613929a72fbc3d695190dc
Yura Kocyuba <yurakocyuba@yandex.ru>
2019-08-08 00:02:38 +0300

    added /v2/history/get_actions endpoint

--- services/get_actions/main.py
+++ services/get_actions/main.py
@@ -4,32 +4,39 @@ import os
 import falcon
 
 # Load Environmental Variables
-full_api = os.environ['GET_ACTIONS_ENDPOINT']
-limited_api = os.environ['GET_ACTIONS_LIMITED_ENDPOINT']
-history_per_account = int(os.environ['HISTORY_PER_ACCOUNT'])
+full_api = os.environ.get('GET_ACTIONS_ENDPOINT', "https://eos.greymass.com")
 
 class GetActions:
     def on_post(self, req, resp):
+        position = -1
+        offset = -500
         request = json.loads(req.stream.read())
         # Retrieve Variables
         account_name = request.get('account_name')
-        offset = int(request.get('offset'))
-        position = int(request.get('pos'))
+        # offset = int(request.get('offset'))
+        # position = int(request.get('pos'))
         # Form the default endpoint path
         endpoint = full_api + '/v1/history/get_actions'
         # Determine if this query fits in the limit history dataset
-        if position == -1 and -history_per_account <= offset <= -1:
-            # If so, request the data from the limited node
-            endpoint = limited_api + '/v1/history/get_actions'
         # Create Request
         r = requests.post(endpoint, json={
             "account_name": account_name,
             "pos": position,
             "offset": offset
         })
+        jsonResponse = json.loads(r.text)
+        actions = jsonResponse['actions']
+        actionsTransfer = []
+        for action in actions:
+            if action['action_trace']['act']['name'] == "transfer":
+                actionsTransfer.append(action)
+
+        response = {"actions": actionsTransfer}
         # Return response
-        resp.body = r.text
+        resp.body = json.dumps(response)
 
 # Launch falcon API
 app = falcon.API()
 app.add_route('/v1/history/get_actions', GetActions())
+app.add_route('/v2/history/get_actions', GetActions())
+
